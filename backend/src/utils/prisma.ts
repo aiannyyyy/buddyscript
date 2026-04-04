8c7da74453e092735888b71a9c6172b5fbf3b141
John Adrian M. Ticatic <john.ticatic@gmail.com>
2026-04-04 10:22:10 +0800

    fixing env

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -8,7 +8,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaPg({ connectionString });
 
 export const prisma =
   globalForPrisma.prisma ??
